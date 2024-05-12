70a040d4afacc647ffe880c5bd701f7be80b9d6a
Saikat Dey <30950554+deysaikat04@users.noreply.github.com>
2024-05-12 12:21:19 +0530

    feat: submit product form added

--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -1,23 +1,27 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 import { formatCurrency } from "@/lib/formatter";
 import { Label } from "@radix-ui/react-label";
 import { useState } from "react";
+import { addProduct } from "../../_actions/products";
 
 function ProductForm() {
   const [priceInCents, setPriceInCents] = useState<number>();
   return (
-    <form className="space-y-8">
+    <form action={addProduct} className="space-y-8">
       <div className="space-y-2">
         <Label htmlFor="name">Name</Label>
-        <Input type="text" id="name" required />
+        <Input type="text" id="name" name="name" required />
       </div>
       <div className="space-y-2">
         <Label htmlFor="name">Price In Cents</Label>
         <Input
           type="number"
           id="priceInCents"
+          name="priceInCents"
           required
           value={priceInCents}
           onChange={(e) => setPriceInCents(Number(e.target.value) || undefined)}
@@ -26,6 +30,19 @@ function ProductForm() {
           {formatCurrency((priceInCents || 0) / 100)}
         </div>
       </div>
+      <div className="space-y-2">
+        <Label htmlFor="description">Description</Label>
+        <Textarea id="description" name="description" required />
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="file">File</Label>
+        <Input type="file" id="file" name="file" required />
+      </div>
+      <div className="space-y-2">
+        <Label htmlFor="image">Image</Label>
+        <Input type="file" id="image" name="image" required />
+      </div>
+      <Button type="submit">Save</Button>
     </form>
   );
 }
